bfaabb59b7ce7f2ba28e69501e4b59afb0f9c372
Bobyshev Nikita <Gammanik@gmail.com>
2019-11-08 22:33:08 +0300

    fixed 3rd test

--- src/task3.py
+++ src/task3.py
@@ -5,6 +5,10 @@ def implicit_int(cls):
     def deco(s, item):
         return 0
 
+    method = getattr(cls, "__getattr__", None)
+    if callable(method):
+        return cls
+
     new_cls.__getattr__ = deco
 
     return new_cls
