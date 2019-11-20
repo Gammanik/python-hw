b1c98b6a43927c74edf6c72201a37f3a0c298124
Bobyshev Nikita <Gammanik@gmail.com>
2019-11-20 12:10:14 +0300

    add task2

--- src/task2.py
+++ src/task2.py
@@ -8,6 +8,8 @@ class staticmethod:
     https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses/
     видно, что так как задекорированная фукнция будет дескриптором,
     то будем искать ее в словаре класса
+    при обращении к полю класса, в котором находится foobar
+    вызывается метод __getattribute__('foobar')
     """
 
     def __init__(self, function):
@@ -17,8 +19,20 @@ class staticmethod:
         return self.function
 
 
-class classmethod:
-    pass
+class classmethod(object):
+    """метод класса должен еще передавать self первым аргументом функции"""
+
+    def __init__(self, f):
+        self.function = f
+
+    def __get__(self, obj, cls=None):
+        if not cls:
+            cls = type(obj)
+
+        def inner(*args, **kwargs):
+            return self.function(cls, *args, **kwargs)
+
+        return inner
 
 
 class C:
@@ -26,6 +40,7 @@ class C:
     def f(arg1, arg2):
         print(arg1, arg2)
 
+    @classmethod
     def g(self):
         print(self)
 
@@ -33,5 +48,6 @@ class C:
 if __name__ == "__main__":
     print("_" * 50)
     print(C.f(1, 2))
+    print(C().g())
 
 
