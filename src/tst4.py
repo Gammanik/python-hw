5b257ea0f77387258187eefe3c0fa56e36f1dd23
Bobyshev Nikita <Gammanik@gmail.com>
2019-12-14 20:59:56 +0300

    make doreshka

--- src/tst4.py
+++ src/tst4.py
@@ -1,21 +1,32 @@
 import inspect
 from functools import wraps
 
-
-all_private = {}
+unfinished = list()
 
 
 def public(f):
+    if hasattr(f, "is_encapsulated"):
+        raise SyntaxError("tho decorators on fun: %s" % f.__name__)
+
+    f.is_encapsulated = True
     f.is_public = True
     return f
 
 
 def private(f):
+    if hasattr(f, "is_encapsulated"):
+        raise SyntaxError("tho decorators on fun: %s" % f.__name__)
+
+    f.is_encapsulated = True
     f.is_private = True
     return f
 
 
 def protected(f):
+    if hasattr(f, "is_encapsulated"):
+        raise SyntaxError("tho decorators on fun: %s" % f.__name__)
+
+    f._is_encapsulated = True
     f.is_protected = True
     return f
 
@@ -23,92 +34,34 @@ def protected(f):
 def call_private(f):
     @wraps(f)
     def inner(*args, **kwargs):
-        print("fun called: ", f.__name__)
-        # print("stack: ", f.__name__, inspect.stack())
-
-
-
         stack = inspect.stack()
-        f_name = stack[1][3]
+        caller_name = stack[1][3]
 
-        print(f_name)
-        print("stack: ", stack)
-        # print(inspect.getsourcelines(f))
-        mem = inspect.getmembers(f)
-        # print(mem.f_code)
+        if hasattr(f, "is_private") or hasattr(f, "is_protected"):
+            if len(unfinished) == 0:
+                raise TypeError("protected method %s could not be called from here"
+                          % f.__name__)
 
-        print()
+            lst_unfin = unfinished[-1]
+            if lst_unfin.__name__ != caller_name or lst_unfin.cls_name != f.cls_name:
+                raise TypeError("protected method %s could not be called from here"
+                          % f.__name__)
 
+        unfinished.append(f)
+        res = f(*args, **kwargs)
+        unfinished.pop()
 
-        # if <module>
-        if f_name not in all_private:
-            TypeError("private method" + f_name + "could not be called from here")
-
-        if f_name == "<module>":
-            TypeError("private method" + f_name + "could not be called from here")
-
-
-        if len(stack) > 1:
-            pass
-
-            # call_private(stack[1][3])
-
-        return f(*args, **kwargs)
+        return res
 
     inner.__name__ = f.__name__
     return inner
 
 
-
 class Encapsulator(type):
-
     def __new__(mcs, name, bases, attrs, **kwargs):
-        # print("__new__ attrs", attrs)
-
         for atr in attrs:
-            val = attrs[atr]
-
-            # if (hasattr(val, "is_private") \
-            #         and (hasattr(val, "is_protected") or hasattr(val, "is_public")):
-            #     raise AttributeError("shouldn't have more than 1 deco: ", atr.__name__)
-
-            if hasattr(val, "is_private"):
-                all_private[atr] = call_private(attrs[atr])
+            if callable(attrs[atr]):
+                attrs[atr].cls_name = name
                 attrs[atr] = call_private(attrs[atr])
 
         return super().__new__(mcs, name, bases, attrs)
-
-
-class A(metaclass=Encapsulator):
-
-    @private
-    def foo_pr3(self):
-        pass
-
-    @private
-    def foo_pr2(self):
-        self.foo_pr3()
-        pass
-
-    @private
-    def foo_pr(self):
-        self.foo_pr2()
-        pass
-
-    def fooCall(self):
-        self.foo_pr()
-
-
-class B(A):
-    pass
-
-
-if __name__ == "__main__":
-    print('*'*60)
-
-    a = A()
-    print(dir(a))
-    # a.foo()
-    a.fooCall()
-    a.foo_pr3()
-
