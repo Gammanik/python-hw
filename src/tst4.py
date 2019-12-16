ca0c14f450eafade3f082ba61a94028c0234e138
Bobyshev Nikita <Gammanik@gmail.com>
2019-12-16 22:13:28 +0300

    add for static and class methods

--- src/tst4.py
+++ src/tst4.py
@@ -1,8 +1,6 @@
 import inspect
 from functools import wraps
 
-unfinished = list()
-
 
 def public(f):
     if hasattr(f, "is_encapsulated"):
@@ -38,30 +36,66 @@ def call_private(f):
         caller_name = stack[1][3]
 
         if hasattr(f, "is_private") or hasattr(f, "is_protected"):
-            if len(unfinished) == 0:
+            f.__name__ = f if isinstance(f, staticmethod) or \
+                              isinstance(f, classmethod) else f.__name__
+
+            if not Encapsulator.unfinished == 0:
                 raise TypeError("protected method %s could not be called from here"
                           % f.__name__)
 
             lst_unfin = unfinished[-1]
             if lst_unfin.__name__ != caller_name or lst_unfin.cls_name != f.cls_name:
-                raise TypeError("protected method %s could not be called from here"
+                raise TypeError("method %s could not be called from here"
                           % f.__name__)
 
-        unfinished.append(f)
+        Encapsulator.unfinished.append(f)
         res = f(*args, **kwargs)
-        unfinished.pop()
+        Encapsulator.unfinished.pop()
 
         return res
 
-    inner.__name__ = f.__name__
     return inner
 
 
 class Encapsulator(type):
+    unfinished = list()
+
     def __new__(mcs, name, bases, attrs, **kwargs):
         for atr in attrs:
-            if callable(attrs[atr]):
-                attrs[atr].cls_name = name
+            print(type(attrs[atr]))
+
+            if callable(attrs[atr]) or isinstance(attrs[atr], staticmethod)\
+                    or isinstance(attrs[atr], classmethod):
+                print(atr)
+                setattr(attrs[atr], 'cls_name', name)
                 attrs[atr] = call_private(attrs[atr])
 
         return super().__new__(mcs, name, bases, attrs)
+
+
+class A(metaclass=Encapsulator):
+
+    @private
+    @staticmethod
+    def st_foo1():
+        pass
+
+    @private
+    @classmethod
+    def cls_foo1(cls):
+        pass
+
+    @private
+    def foo1(self):
+        pass
+
+
+if __name__ == "__main__":
+
+    A.st_foo1()
+    a = A()
+    # a.foo1()
+    # a.cls_foo1()
+    # a.st_foo1()
+
+
