73f147b8f7f600253f7b12d2e4b39a0c4d545215
Bobyshev Nikita <Gammanik@gmail.com>
2019-12-25 13:42:07 +0300

    continue task2

--- src/task2.py
+++ src/task2.py
@@ -1,25 +1,57 @@
 
 
+# todo: inside ht
 def h(item, md):
-    return divmod(hash(item), md)[1]
+    return hash(item) % md
 
 
 class HashTable:
     def __init__(self):
-        self.mod = 199
-        self.lst = [None] * self.mod
+        self.sz = 101
+        self.lst = [None] * self.sz
+        self.deleted = [True] * self.sz
         self.elems = 0
 
     def __getitem__(self, item):
-        return self.lst[h(item, self.mod)][0]
+        if self.deleted[h(item, self.sz)]:
+            raise KeyError(item)
+
+        return self.lst[h(item, self.sz)][-1][1]
 
     def __setitem__(self, key, value):
         self.elems += 1
 
-        if not self.lst[h(key, self.mod)]:
-            self.lst[h(key, self.mod)] = []
+        if not self.lst[h(key, self.sz)]:
+            self.lst[h(key, self.sz)] = []
 
-        self.lst[h(key, self.mod)].append(value)
+        self.deleted[h(key, self.sz)] = False
+        self.lst[h(key, self.sz)].append((key, value))
+
+    def __delitem__(self, key):
+        self.elems -= 1
+        self.deleted[h(key, self.sz)] = True
+
+        if len(self.lst[h(key, self.sz)]) == 1:
+            del self.lst[h(key, self.sz)]
+        else:
+            del self.lst[h(key, self.sz)][-1]
+
+    def keys(self):
+        # return [[self.lst[i][j] for j in range(len(self.lst))] for i in range(self.sz) if not self.deleted[i]]
+        # return [self.lst[i] for i in range(self.sz) if not self.deleted[i]]
+        for i in range(0, self.sz):
+            if not self.deleted[i]:
+                for j in self.lst[i]:
+                    yield j[0]
+
+    def items(self):
+        for i in range(0, self.sz):
+            if not self.deleted[i]:
+                for j in self.lst[i]:
+                    yield j
+
+    def __contains__(self, item):
+        return not self.deleted[h(item, self.sz)]
 
 
 if __name__ == "__main__":
@@ -28,5 +60,31 @@ if __name__ == "__main__":
 
     htable = HashTable()
     htable['qwe'] = 42
+    htable['qwe'] = 55
+    htable['ex'] = 40
     print(htable['qwe'])
+    print()
+
+    # del htable['qwe']
+    # print(htable['qwe'])
+
+    # print(htable.keys())
+    # print()
+
+    print('a' in htable)
+    print('qwe' in htable)
+
+    for key in htable.keys():
+        print(key)
+
+    for key, value in htable.items():
+        print(key, value)
+
+
+        
+
+
+
+
+
 
