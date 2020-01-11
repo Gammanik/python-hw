166b5402cf78b2eb3e3e4f710b1e94caf6bf850e
Bobyshev Nikita <Gammanik@gmail.com>
2020-01-11 15:23:34 +0300

    rewrite from scratch

--- src/task1.py
+++ src/task1.py
@@ -1,154 +1,64 @@
 from functools import wraps
 
-
 unfinished = list()
 
 
 def deco(f):
     @wraps(f)
-    def inner(*args, **kwargs):
-        # print("bef: ", list(map(lambda ls: ls[2], Dictator.cls_inst)), f.__name__)
-        num_before = len(Dictator.cls_inst)
-        unfinished.append((num_before, f, args[0]))
-        val = f(*args, **kwargs)
-        num_after = len(Dictator.cls_inst)
-        # print("after: ", list(map(lambda ls: ls[2], Dictator.cls_inst)), f.__name__)
-        # print("unfin: ", unfinished[-1][2])
-
-        was_before = unfinished[-1][0]
+    def inner(self, *args, **kwargs):
+        _id = self.id if f.__name__ != '__init__' else self.last_id + 1
+        unfinished.append((self, f'{f.__name__}', _id))
+        val = f(self, *args, **kwargs)
         unfinished.pop()
-
-
-        if num_after > was_before:
-            slf = args[0]
-            for i in range(0, len(Dictator.cls_inst)):
-                if slf.id == Dictator.cls_inst[i][2]:
-                    pass
-                    # Dictator.cls_inst[unfinished[-1][2].id][1].append("-> %s.%s" % (slf.__class__.__name__, f.__name__))
-                    # Dictator.cls_inst[i][1].append("-> %s.%s" % (slf.__class__.__name__, f.__name__))
         return val
-
     return inner
 
 
+def cls_log(cls):
+    for pair in cls.instances:
+        inst = pair[0]
+        str_rep = f"{inst.__class__.__name__}.id{inst.id}: {inst}"
+        if pair[1]:
+            str_rep += f' | {pair[1]}'
+        yield str_rep
+
+
 class Dictator(type):
-    curr_id = 0
-    cls_inst = list()
-
-    def show_log(cls=None):
-        if not cls:
-            yield from map(lambda ls: " ".join(ls[1]), Dictator.cls_inst)
-        else:
-            if not isinstance(cls, Dictator):
-                raise TypeError("class %s should be a metaclass of %s" % (cls.__name__, Dictator.__name__))
-
-            yield from [" ".join(x[1]) for x in list(filter(lambda obj: obj[0].__class__ == cls, Dictator.cls_inst))]
-
+    reg_cls = {}
 
     @staticmethod
-    def clear_log():
-        Dictator.cls_inst.clear()
+    def show_log(cls=None):
+        if cls:
+            yield from cls_log(cls)
+        else:
+            for reg_cls in Dictator.reg_cls:
+                yield from cls_log(reg_cls)
 
-    def __new__(mcs, name, bases, attrs):
-        # print("new %s %s" % (mcs.__name__, name))
+    @staticmethod
+    def clear_log(cls=None):
+        if cls:
+            del Dictator.reg_cls[cls]
+            del cls.instances
+        else:
+            for _c in Dictator.reg_cls:
+                del _c.instances
+            Dictator.reg_cls.clear()
 
-        for key, item in attrs.items():
-            if callable(item):
-                attrs[key] = deco(attrs[key])
-                # attrs[key].class_name = name
-            if key == '__init__':
-                print('__init: ')
-                attrs[key] = deco(attrs[key])
+    def __new__(mcs, name, bases, attrs, **kwargs):
+        for k, fun in attrs.items():
+            if callable(fun):
+                attrs[k] = deco(attrs[k])
 
         cls = super().__new__(mcs, name, bases, attrs)
-        # cls.id = Dictator.curr_id
+        cls.last_id = 0
+        cls.instances = []
         return cls
 
     def __call__(cls, *args, **kwargs):
-        Dictator.curr_id += 1
-        # print("call %s %s id:%s" % (cls.__name__, args, Dictator.curr_id))
-        obj = super().__call__(*args, **kwargs)
-        obj.id = Dictator.curr_id
-
-        lst_rep = list()
-        lst_rep.append("id:%s" % Dictator.curr_id)
-        lst_rep.append(str(obj))
-        # str_rep = " ".join(lst_rep)
-        str_rep = lst_rep
-
-        Dictator.cls_inst.append((obj, str_rep, Dictator.curr_id))
-        return obj
-
-
-# class A(metaclass=Dictator):
-#     pass
-#
-#
-# class B(metaclass=Dictator):
-#     pass
-
-
-if __name__ == "__main__":
-    # print('*'*60)
-    # a = A()
-    # aa = A()
-    # b = B()
-    #
-    # for log in Dictator.show_log(A):
-    #     print(log)
-
-    Dictator.clear_log()
-
-    class A(metaclass=Dictator):
-        def __init__(self, x):
-            print(x)
-
-
-    class B(metaclass=Dictator):
-        def foo(self, y):
-            tmp = A(y)
-
-        def bar(self):
-            self.foo(3)
-
-
-    b = B()
-    print('b id: ', b.id)
-    b.bar()
-
-    for log in Dictator.show_log():
-        print(log)
-
-    # Dictator.clear_log()
-    #
-    #
-    # class A(metaclass=Dictator):
-    #     def __init__(self, b):
-    #         b.bar()
-    #
-    #
-    # class B(metaclass=Dictator):
-    #     def foo(self, y):
-    #         tmp = C()
-    #
-    #     def bar(self):
-    #         self.foo(3)
-    #
-    #
-    # class C(metaclass=Dictator):
-    #     pass
-    #
-    #
-    # c = C()
-    # b1 = B()
-    # b2 = B()
-    # a = A(b2)
-    #
-    # for log in Dictator.show_log():
-    #     print(log)
-
-
-
-
-
-
+        inst = super().__call__(*args, **kwargs)
+        cls.last_id += 1
+        inst.id = cls.last_id
+        str_unfin = " -> ".join(map(lambda p: f'{p[0].__class__.__name__}.id{p[2]}.{p[1]}', unfinished))
+        cls.instances.append((inst, None if not len(str_unfin) else str_unfin))
+        Dictator.reg_cls[cls] = cls
+        return inst
